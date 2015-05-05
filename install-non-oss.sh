9dc104b29a0d6d0e785f048a06c9592cc9094549
Rohit Yadav <rohit.yadav@shapeblue.com>
2015-05-05 16:37:25 +0200

    add vim25 from 6.0 sdk
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- install-non-oss.sh
+++ install-non-oss.sh
@@ -22,6 +22,7 @@ mvn install:install-file -Dfile=cloud-netscaler-sdx.jar -DgroupId=com.cloud.com.
 # From https://my.vmware.com/group/vmware/get-download?downloadGroup=VSP510-WEBSDK-510
 # Version: 5.1, Release-date: 2012-09-10, Build: 774886
 mvn install:install-file -Dfile=vim25_51.jar        -DgroupId=com.cloud.com.vmware -DartifactId=vmware-vim25    -Dversion=5.1   -Dpackaging=jar
+mvn install:install-file -Dfile=vim25_60.jar        -DgroupId=com.cloud.com.vmware -DartifactId=vmware-vim25    -Dversion=6.0   -Dpackaging=jar
 
 # From http://support.netapp.com/  (not available online, contact your support representative)
 # Version: 4.0
