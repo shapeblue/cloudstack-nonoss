76b7e947717c3adcd81fcb69ab90c8ae0e52c6be
Rohit Yadav <rohit@yadav.cloud>
2018-07-02 14:41:51 +0530

    add vim sdk for 6.7
    Signed-off-by: Rohit Yadav <rohit@apache.org>

--- install-non-oss.sh
+++ install-non-oss.sh
@@ -30,6 +30,7 @@ mvn install:install-file -Dfile=vim25_51.jar        -DgroupId=com.cloud.com.vmwa
 mvn install:install-file -Dfile=vim25_55.jar        -DgroupId=com.cloud.com.vmware -DartifactId=vmware-vim25    -Dversion=5.5   -Dpackaging=jar
 mvn install:install-file -Dfile=vim25_60.jar        -DgroupId=com.cloud.com.vmware -DartifactId=vmware-vim25    -Dversion=6.0   -Dpackaging=jar
 mvn install:install-file -Dfile=vim25_65.jar        -DgroupId=com.cloud.com.vmware -DartifactId=vmware-vim25    -Dversion=6.5   -Dpackaging=jar
+mvn install:install-file -Dfile=vim25_67.jar        -DgroupId=com.cloud.com.vmware -DartifactId=vmware-vim25    -Dversion=6.7   -Dpackaging=jar
 
 # From http://support.netapp.com/  (not available online, contact your support representative)
 # Version: 4.0 (http://community.netapp.com/t5/Developer-Network-Articles-and-Resources/NetApp-Manageability-NM-SDK-Introduction-and-Download-Information/ta-p/86418)
