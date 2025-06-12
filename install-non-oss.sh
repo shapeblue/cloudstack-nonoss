1fa817ea690c6bebf724691f88c3f28b2d402038
Wei Zhou <weizhou@apache.org>
2025-06-12 11:02:18 +0200

    Update -Dversion to 1.0.9-1

--- install-non-oss.sh
+++ install-non-oss.sh
@@ -72,4 +72,4 @@ mvn install:install-file -Dfile=nsx-policy-java-sdk-4.1.0.2.0.jar -DgroupId=com.
 mvn install:install-file -Dfile=netris-java-sdk-1.0.0.jar -DgroupId=io.netris -DartifactId=netris-java-sdk -Dversion=1.0.0 -Dpackaging=jar
 
 # Install new JAR for Nested-oobm CloudStack plugin
-mvn install:install-file -Dfile=apache-cloudstack-java-client-1.0.9-new.jar -DgroupId=br.com.autonomiccs -DartifactId=apache-cloudstack-java-client -Dversion=1.0.9 -Dpackaging=jar
+mvn install:install-file -Dfile=apache-cloudstack-java-client-1.0.9-new.jar -DgroupId=br.com.autonomiccs -DartifactId=apache-cloudstack-java-client -Dversion=1.0.9-1 -Dpackaging=jar
